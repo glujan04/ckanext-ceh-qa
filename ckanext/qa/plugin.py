8fb33ac3a46bb07cfdda60ebcd40c013a5931d7e
glujan <glujan@idom.com>
2020-06-09 06:43:41 -0500

    qa

---
+++ ckanext/qa/plugin.py
@@ -0,0 +1,114 @@
+import logging
+
+import ckan.model as model
+import ckan.plugins as p
+
+from ckanext.archiver.interfaces import IPipe
+from logic import action, auth
+from model import QA, aggregate_qa_for_a_dataset
+import helpers
+import lib
+from ckanext.report.interfaces import IReport
+
+
+log = logging.getLogger(__name__)
+
+
+class QAPlugin(p.SingletonPlugin, p.toolkit.DefaultDatasetForm):
+    p.implements(p.IConfigurer, inherit=True)
+    p.implements(p.IRoutes, inherit=True)
+    p.implements(IPipe, inherit=True)
+    p.implements(IReport)
+    p.implements(p.IActions)
+    p.implements(p.IAuthFunctions)
+    p.implements(p.ITemplateHelpers)
+    p.implements(p.IPackageController, inherit=True)
+
+    # IConfigurer
+
+    def update_config(self, config):
+        p.toolkit.add_template_directory(config, 'templates')
+
+    # IRoutes
+
+    def before_map(self, map):
+        # Link checker - deprecated
+        res = 'ckanext.qa.controllers:LinkCheckerController'
+        map.connect('qa_resource_checklink', '/qa/link_checker',
+                    conditions=dict(method=['GET']),
+                    controller=res,
+                    action='check_link')
+        return map
+
+    # IPipe
+
+    def receive_data(self, operation, queue, **params):
+        '''Receive notification from ckan-archiver that a dataset has been
+        archived.'''
+        if not operation == 'package-archived':
+            return
+        dataset_id = params['package_id']
+
+        dataset = model.Package.get(dataset_id)
+        assert dataset
+
+        lib.create_qa_update_package_task(dataset, queue=queue)
+
+    # IReport
+
+    def register_reports(self):
+        """Register details of an extension's reports"""
+        from ckanext.qa import reports
+        return [reports.openness_report_info]
+
+    # IActions
+
+    def get_actions(self):
+        return {
+            'qa_resource_show': action.qa_resource_show,
+            'qa_package_openness_show': action.qa_package_openness_show,
+            }
+
+    # IAuthFunctions
+
+    def get_auth_functions(self):
+        return {
+            'qa_resource_show': auth.qa_resource_show,
+            'qa_package_openness_show': auth.qa_package_openness_show,
+            }
+
+    # ITemplateHelpers
+
+    def get_helpers(self):
+        return {
+            'qa_openness_stars_resource_html':
+            helpers.qa_openness_stars_resource_html,
+            'qa_openness_stars_dataset_html':
+            helpers.qa_openness_stars_dataset_html,
+            }
+
+    # IPackageController
+
+    def after_show(self, context, pkg_dict):
+        # Insert the qa info into the package_dict so that it is
+        # available on the API.
+        # When you edit the dataset, these values will not show in the form,
+        # it they will be saved in the resources (not the dataset). I can't see
+        # and easy way to stop this, but I think it is harmless. It will get
+        # overwritten here when output again.
+        qa_objs = QA.get_for_package(pkg_dict['id'])
+        if not qa_objs:
+            return
+        # dataset
+        dataset_qa = aggregate_qa_for_a_dataset(qa_objs)
+        pkg_dict['qa'] = dataset_qa
+        # resources
+        qa_by_res_id = dict((a.resource_id, a) for a in qa_objs)
+        for res in pkg_dict['resources']:
+            qa = qa_by_res_id.get(res['id'])
+            if qa:
+                qa_dict = qa.as_dict()
+                del qa_dict['id']
+                del qa_dict['package_id']
+                del qa_dict['resource_id']
+                res['qa'] = qa_dict
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from ckanext.qa import __version__
+
+setup(
+    name='ckanext-qa',
+    version=__version__,
+    description='Quality Assurance plugin for CKAN',
+    long_description='',
+    classifiers=[],
+    keywords='',
+    author='Open Knowledge Foundation, Cabinet Office & contributors',
+    author_email='info@okfn.org',
+    url='http://ckan.org/wiki/Extensions',
+    license='mit',
+    packages=find_packages(exclude=['tests']),
+    namespace_packages=['ckanext'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'ckanext-archiver>=2.0',
+        'ckanext-report',
+        'SQLAlchemy>=0.6.6',
+        'requests',
+        'xlrd>=0.8.0',
+        'messytables>=0.8',
+        'python-magic>=0.4',
+        'progressbar',
+        'six>=1.9' # until messytables->html5lib releases https://github.com/html5lib/html5lib-python/pull/301
+    ],
+    tests_require=[
+        'nose',
+        'mock',
+        'flask'
+    ],
+    entry_points='''
+    [paste.paster_command]
+    qa=ckanext.qa.commands:QACommand
+
+    [ckan.plugins]
+    qa=ckanext.qa.plugin:QAPlugin
+
+    [ckan.celery_task]
+    tasks=ckanext.qa.celery_import:task_imports
+    ''',
+)
