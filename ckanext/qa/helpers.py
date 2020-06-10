19423140d1b0eec72f66911987f5efaf84402db3
glujan <glujan@idom.com>
2020-06-10 01:36:35 -0500

    qa

--- ckanext/qa/helpers.py
+++ ckanext/qa/helpers.py
@@ -55,6 +55,7 @@ def qa_openness_stars_dataset2_html(dataset):
     id_ = getattr(dataset, 'id')
     pkg = model.Package.get(id_)
     print 'Package %s %s' % (pkg.name, pkg.id)
+    global _RESOURCE_FORMAT_SCORES
     _RESOURCES = {}
     score = 1
     for res in pkg.resources:
