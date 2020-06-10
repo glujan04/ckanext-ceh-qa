5cf4a1d27b3ea00f19162fcd5af0e8924732506e
glujan <glujan@idom.com>
2020-06-09 23:15:21 -0500

    qa

--- ckanext/qa/helpers.py
+++ ckanext/qa/helpers.py
@@ -62,7 +62,7 @@ def qa_openness_stars_dataset2_html(dataset):
         #print format_
         if format_ in _RESOURCES:
             raise ValueError('Formato duplicado %s' % format_)
-        #_RESOURCES[format_] = jsonFormats_[format_]
+        _RESOURCES[format_] = jsonFormats_[format_]
     print _RESOURCES
     if not qa:
         return tk.literal('<!-- No qa info for this dataset -->')
