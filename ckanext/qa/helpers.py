3b0cb34e0204831be0e9d1b4eb5bd049ceff88f7
glujan <glujan@idom.com>
2020-06-10 00:11:55 -0500

    qa

--- ckanext/qa/helpers.py
+++ ckanext/qa/helpers.py
@@ -58,7 +58,7 @@ def qa_openness_stars_dataset2_html(dataset):
     _RESOURCES = {}
     for res in pkg.resources:
         #print res format
-        formato = getattr(res, 'format').upper().strip()
+        formato = string.strip(getattr(res, 'format').upper())
         print formato
         x = getattr(jsonFormats, formato, None)
         print (x)
