c8a045203ca3baa85f5a220f8967155b29b4af1f
glujan <glujan@idom.com>
2020-07-07 13:10:45 -0500

    07-07-2020

--- ckanext/qa/helpers.py
+++ ckanext/qa/helpers.py
@@ -98,7 +98,7 @@ def qa_openness_stars_dataset2_html(dataset):
     if not isinstance(qa, dict):
         return tk.literal('<!-- QA info was of the wrong type -->')
 
-    QAPlugin = QAPlugin()
+    QAPlugin_ = QAPlugin()
     schema = QAPlugin_.update_package_schema()
     print schema
     #schema = self._modify_package_schema(schema)
