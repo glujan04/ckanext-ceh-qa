8ff6859d6114ca2deb19202ce761e7652ea52c90
glujan <glujan@idom.com>
2020-07-07 00:28:09 -0500

    06-07-2020

--- ckanext/qa/helpers.py
+++ ckanext/qa/helpers.py
@@ -94,7 +94,7 @@ def qa_openness_stars_dataset2_html(dataset):
         lb_star = 'estrella'
     qa = {'openness_score': _RESOURCES[maximum], 'openness_score_reason': 'El conjunto de datos recibe una puntuacion de {0} {1} al disponer de un recurso en formato \"{2}\".'.format(_RESOURCES[maximum], lb_star, maximum),
                'updated': None}
-    print 'puntuación'.encode('ascii')
+    print u'puntuación'
     if not qa:
         return tk.literal('<!-- No qa info for this dataset -->')
     if not isinstance(qa, dict):
