83a77d5e84c136622a0d08ac7912ebfa580001a8
glujan <glujan@idom.com>
2020-07-07 11:58:38 -0500

    07-07-2020

--- ckanext/qa/helpers.py
+++ ckanext/qa/helpers.py
@@ -4,6 +4,7 @@ from ckan.plugins import toolkit as tk
 import lib
 import os
 from ckan import model
+import ckanext.qa.plugin as QAPlugin
 
 
 def qa_openness_stars_resource_html(resource):
@@ -97,6 +98,9 @@ def qa_openness_stars_dataset2_html(dataset):
     if not isinstance(qa, dict):
         return tk.literal('<!-- QA info was of the wrong type -->')
 
+    schema = super(QAPlugin, self).update_package_schema()
+    print schema
+    #schema = self._modify_package_schema(schema)
     # Take a copy of the qa dict, because weirdly the renderer appears to add
     # keys to it like _ and app_globals. This is bad because when it comes to
     # render the debug in the footer those extra keys take about 30s to render,
